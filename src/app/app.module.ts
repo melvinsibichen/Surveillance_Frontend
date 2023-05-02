b69cd162ed134dafb04ebe3354d8397199add891
melvinsibichen <melvinsibichen1020@gmail.com>
2023-05-02 11:05:03 +0530

    adde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
